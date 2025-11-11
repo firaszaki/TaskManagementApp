569d0abfe5b986d0d816fa30fe3a6b1a2d65a796
Firas Zaki <fiiraas01@gmail.com>
2025-11-11 08:17:12 +0800

    update: add components (index, details and form)

--- TaskManagementApp.Application/Repositories/TaskRepository.cs
+++ TaskManagementApp.Application/Repositories/TaskRepository.cs
@@ -38,4 +38,69 @@ public class TaskRepository
 
         return new ProcessResult<TaskEntity?>(false, "Data not found.", null);
     }
+
+    public async Task<IProcessResult<long>> AddAsync(TaskEntity input)
+    {
+        using var context = dbContextFactory.CreateDbContext();
+
+        input.CreatedAt = DateTime.Now;
+
+        await context.Tasks.AddAsync(input);
+        await context.SaveChangesAsync();
+
+        var insertedId = input.Id;
+        return new ProcessResult<long>(true, "Successfully inserted data.", insertedId);
+    }
+
+    public async Task<IProcessResult> UpdateAsync(TaskEntity input)
+    {
+        try
+        {
+            using var context = dbContextFactory.CreateDbContext();
+
+            if(input.Status == Domain.Entities.TaskStatus.Completed)
+            {
+                input.IsCompleted = true;
+            }
+            else
+            {
+                input.IsCompleted = false;
+            }
+
+            context.Entry(input).State = EntityState.Modified;
+            await context.SaveChangesAsync();
+            return new ProcessResult(true, "Successfully updated task.");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.ToString());
+            logger.LogError(ex.ToString(), "Failed to update task.");
+            return new ProcessResult(false, "Failed to update task.");
+        }
+    }
+
+    public async Task<IProcessResult> DeleteAsync(long id)
+    {
+        try
+        {
+            using var context = dbContextFactory.CreateDbContext();
+
+            var entity = await context.Tasks.FirstOrDefaultAsync(c => c.Id == id);
+            if (entity == null)
+            {
+                return new ProcessResult(false, "Task not found");
+            }
+
+            context.Entry(entity).State = EntityState.Deleted;
+            await context.SaveChangesAsync();
+            return new ProcessResult(true, "Successfully deleted.");
+
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.ToString());
+            logger.LogError(ex.ToString());
+            return new ProcessResult(false, "Failed to delete record.");
+        }
+    }
 }
